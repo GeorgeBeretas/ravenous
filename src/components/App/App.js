59a16b6fdd0d221f9a87ddcee9a0a33b326981cb
GeorgeBeretas <g.beretas88@gmail.com>
2018-11-14 16:55:46 +0000

    no message

---
+++ src/components/App/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
